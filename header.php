8aba328758502bed64f5cc7eb67637e883993e2d
Ryan <RyanMaddockBusiness@outlook.com>
2020-02-01 12:44:38 +0000

    Started styling portfolio

---
+++ header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Ryan Maddock Portfolio</title>
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="flex-container navigation">
+      <div class="flex-container">
+        <div class="logo"><img src="images/LOGO.png"/></div>
+        <div class="links flex-container">
+          <a>Work</a>
+          <a>About</a>
+          <a>Contact</a>
+        </div>
+      </div>
+
+</div>
---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+include 'header.php';
+?>
+<div class="flex-container content-holder">
+  <div class="content">
+    <div class="section welcome flex-container">
+      <div class="section me flex-container">
+        <span></span>
+        <div>
+          <h1><span>R</span>yan <span>M</span>addock</h1>
+          <p class="cen">Junior Front End Developer</p>
+        </div>
+
+      </div>
+    </div>
+    <div class="section skills">
+      <div class="sectionTitle">My Skills</div>
+      <div class="progress flex-container">
+        <div>
+          HTML
+          <span></span>
+        </div>
+        <div>
+          CSS
+          <span></span>
+        </div>
+        <div>
+          JS
+          <span></span>
+        </div>
+        <div>
+          PHP
+          <span></span>
+      </div>
+      </div>
+    </div>
+    <div class="section contact">
+
+    </div>
+
+  </div>
+
+</div>
+<?php
+include 'footer.php';
+ ?>
